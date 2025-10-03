afadb7ce1f7403b8d800220b0c405a50755c135d
zhangsan <1299940308@qq.com>
2025-10-03 23:45:37 +0800

    Update AnyExt.kt

--- app/src/main/java/lj/sword/demoappnocompose/ext/AnyExt.kt
+++ app/src/main/java/lj/sword/demoappnocompose/ext/AnyExt.kt
@@ -18,3 +18,8 @@ fun Any.toJson(): String = JsonUtil.toJson(this)
  * 对象转 JSON 字符串（格式化）
  */
 fun Any.toPrettyJson(): String = JsonUtil.toPrettyJson(this)
+
+/**
+ * 任意对象是否为空
+ */
+fun Any?.isNullOrEmpty(): Boolean = this == null || this.toString().isNullOrEmpty()
